7173c317e5201b74ea574e2784c5e34061c1b2c1
zxyfanta <13980774780@139.com>
2025-10-20 12:58:43 +0800

    修复bug

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -15,7 +15,7 @@ axiosRequest.interceptors.request.use(
     (config)=>{
         const token=localStorage.getItem('token')
         if(token){
-            config.headers.Authorization='Bearer ${token}'
+            config.headers.Authorization=`Bearer ${token}`
         }
         return config
     },
